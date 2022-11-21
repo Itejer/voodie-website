f698b7f8e70ebda1403fbccf34e090293209ecbd
Itejer <117469283+Itejer@users.noreply.github.com>
2022-11-21 01:28:55 -0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+var menuList = document.getElementById('menuList');
+
+
+function openNav(){
+if(menuList.style.display == 'none'){
+    menuList.style.display = 'block';
+}
+else{
+    menuList.style.display = 'none';
+}
+
+
+}
